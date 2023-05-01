3efac801256f0958b25b4cdc19ba385e2711bf80
Solovicky Bohdan <bohdan.solovicky@gmail.com>
2023-05-01 18:18:39 +0300

    Fix problems with  printf

--- cmd/ffbot.go
+++ cmd/ffbot.go
@@ -30,7 +30,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("fbot %s started", appVersion)
+		fmt.Printf("fbot %s started", appVersion)
 
 		ffbot, err := telebot.NewBot(telebot.Settings{
 			URL:    "",
